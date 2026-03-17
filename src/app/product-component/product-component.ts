079f337a0675c30fb2d0464197ab68b38b987a9a
glaucotodesco <glauco.todesco@gmail.com>
2026-03-17 10:34:49 -0300

    aula 17-03

--- src/app/product-component/product-component.ts
+++ src/app/product-component/product-component.ts
@@ -1,31 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Product } from '../product';
+import { ProductService } from '../product-service';
 
 @Component({
   selector: 'app-product',
   standalone: false,
-  templateUrl: './product.html',
-  styleUrl: './product.css',
+  templateUrl: './product-component.html',
+  styleUrl: './product-component.css',
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
+  formGroupProduct: FormGroup;
 
-   formGroupProduct: FormGroup;
-    products: Product[] = [];
+  products = signal<Product[]>([]);
 
-    constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private service: ProductService,
+  ) {
+    this.formGroupProduct = formBuilder.group({
+      id: [''],
+      name: [''],
+      description: [''],
+      price: [],
+    });
+  }
 
-      this.formGroupProduct = formBuilder.group({
-        id: [''],
-        name: [''],
-        description: [''],
-        price: []
-      });
-    }
+  ngOnInit(): void {
+      this.service.getAllProducts().subscribe(
+          {
+              next: json => this.products.set(json)
+          }
+      );
+  }
 
-    save(){
-      this.products.push(this.formGroupProduct.value);
-      this.formGroupProduct.reset();
-    }
-
+  save() {
+   this.service.save(this.formGroupProduct.value).subscribe(
+     {
+       next: json => {
+          this.products.update(products => [...products, json]);
+          this.formGroupProduct.reset();
+       }
+     }
+    );
+  }
 }
